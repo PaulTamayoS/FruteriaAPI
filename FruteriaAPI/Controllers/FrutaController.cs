e1353d87cf843f26bf6cc97b9efef48fbfe558a4
PaulTamayoS <70984734+PaulTamayoS@users.noreply.github.com>
2023-06-11 22:22:19 +0200

    V3

---
+++ FruteriaAPI/Controllers/FrutaController.cs
@@ -0,0 +1,41 @@
+﻿using FruteriaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FruteriaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FrutaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id) 
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Fruta myFruta)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put(Fruta myFruta) 
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ FruteriaAPI/Models/Fruta.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FruteriaAPI.Models
+{
+    public class Fruta
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Nombre { get; set; }
+        [Required]        
+        public decimal Precio { get; set; }
+        public string Comentarios { get; set; }
+
+    }
+}
